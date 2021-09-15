be111553e205f322c9a6ba96bcb7bf4ea072f1c7
Gilbert Enriquez <gilbertenriquez10@gmail.com>
2021-09-15 14:09:43 +0800

    Add project files.

---
+++ EssentialsExceccise/EssentialsExceccise/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EssentialsExceccise
+{
+    public partial class MainPage : ContentPage
+    {
+        private int num = 0;
+        private string[] words = new string[]
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            AdviceWord.Text = words[num];
+        }
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            num++;
+            if (num >= words.Length)
+                num = 0;
+
+            AdviceWord.Text = words[num];
+        }
+    }
+}
